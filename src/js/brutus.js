9e814ad88ee261aa956ed1e9a45cd8b846f5d782
nox <ts.sarkowicz@gmail.com>
2024-09-24 22:37:42 +0200

    Should be working

--- src/js/brutus.js
+++ src/js/brutus.js
@@ -1,17 +1,47 @@
 function runBrutus(key, message)
 {
-        // get the key and message from the user
-        var key = document.getElementById(key).value;
-        var message = document.getElementById(message).value;
+        const key = document.getElementById(key).value;
+        const message = document.getElementById(message).value;
+
+        if (key == "" || message == "")
+        {
+                document.getElementById("output").innerHTML = "Please enter a key and message.";
+                return;
+        }
+
+        if (key.length < 3)
+        {
+                document.getElementById("output").innerHTML = "The key must be at least 3 characters.";
+                return;
+        }
+
+        if (key.length > 50)
+        {
+                document.getElementById("output").innerHTML = "The key must be at most 50 characters.";
+                return;
+        }
+
+        if (message.length > 50)
+        {
+                document.getElementById("output").innerHTML = "The message must be at most 50 characters.";
+                return;
+        }
 
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/php/brutus.php", true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send("key=" + key + "&message=" + message);
-        xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
+        xhr.onreadystatechange = function() 
+        {
+                if (xhr.readyState == 4 && xhr.status == 200) 
+                {
+                        if (xhr.responseText == "")
+                        {
+                                document.getElementById("output").innerHTML = "";
+                                return;
+                        }
+                        const output = xhr.responseText.slice(0, -1);
                         document.getElementById("output").innerHTML = xhr.responseText;
-                        console.log(xhr.responseText);
                 }
         }
 }
--- src/php/brutus.php
+++ src/php/brutus.php
@@ -1,25 +1,25 @@
 <?php
-function sanitizeInput($data) 
+function sanitizeInput($data)
 {
-	return htmlspecialchars(strip_tags($data), ENT_QUOTES, 'UTF-8');
+        return htmlspecialchars(strip_tags($data), ENT_QUOTES, 'UTF-8');
 }
 
-function sanitizeOutput($data) 
+function sanitizeOutput($data)
 {
-	return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+        return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
 }
 
 function runBrutus($key, $message)
 {
-	$key = sanitizeInput($key);
-	$message = sanitizeInput($message);
-	$output = shell_exec('./../exec/brutus $message $key');
-	$output = sanitizeOutput($output);
-	return $output;
+        $key = escapeshellarg(sanitizeInput($key));
+        $message = escapeshellarg(sanitizeInput($message));
+        $output = shell_exec("./../exec/brutus $message $key");
+        $output = sanitizeOutput($output);
+        return $output;
 }
 
 if (isset($_POST['key']) && isset($_POST['message']))
 {
-	echo runBrutus($_POST['key'], $_POST['message']);
+        echo runBrutus($_POST['key'], $_POST['message']);
 }
 ?>
