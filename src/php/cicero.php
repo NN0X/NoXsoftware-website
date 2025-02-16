1d046415ca60009692290bc8bbc965b016165e20
Tomasz Sarkowicz <nox@noxsoftware.pl>
2025-02-16 21:46:31 +0100

    lol fix

--- src/php/cicero.php
+++ src/php/cicero.php
@@ -9,7 +9,7 @@ function sanitizeOutput($data)
         return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
 }
 
-function runBrutus($message)
+function runCicero($message)
 {
         $message = escapeshellarg(sanitizeInput($message));
         $output = shell_exec("./../exec/cicero -i $message");
